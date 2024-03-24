095574c531ff91bb386a9ca5064af54f8423adfb
Ezequiel Carlos <zequielzico@gmail.com>
2024-03-24 16:26:55 +0000

    Ref: onChange

--- src/core/ref/Ref.ts
+++ src/core/ref/Ref.ts
@@ -1,148 +1,89 @@
-export class Ref2<T> {
+interface CustomEvent {
+  target: WeakRef<any>;
+  propertyKey: string | symbol;
+  fun?: WeakRef<Function>;
+}
+
+export class Ref<T> {
   private _id: string = Math.random().toString(36).substr(2, 9);
-  private _subscribers: { ref: any; name: string; fun?: Function }[] = [];
-  public proxy: any;
+  private _value: T;
+  private registry: FinalizationRegistry<any>;
+  private _subscribers: CustomEvent[] = [];
 
   constructor(value: T) {
-    if (typeof value == "object") {
-      this.proxy = new Proxy(value, {
-        get: function (target, prop) {
-          return target[prop];
-        },
-        set: this.setData.bind(this),
-      });
-      Object.defineProperty(this, "value", {
-        get: function () {
-          return this.proxy;
-        },
-        set: function (v) {
-          this.proxy = v;
-        },
-      });
-
-      this.toString = function (): string {
-        return JSON.stringify(this.proxy);
-      };
-    } else {
-      this.proxy = value;
-      this.toString = function (): string {
-        return this.proxy as any;
-      };
-    }
-  }
-
-  destructor() {}
-
-  private setData(target: any, prop: any, value: any) {
-    this._subscribers.forEach((subscriber) => {
-      if (subscriber.fun) subscriber.fun(this.toString());
-      else subscriber.ref[subscriber.name] = this.toString();
+    this._value = value;
+    this._value = value;
+    this.registry = new FinalizationRegistry((target: WeakRef<any>) => {
+      this._subscribers = this._subscribers.filter(
+        (subscriber) => subscriber.target != target,
+      );
+      console.log("registry: ", target);
     });
-    target[prop] = value;
-    return true;
   }
 
   get value(): T {
-    return this.proxy;
+    return this._value;
   }
 
   set value(v: T) {
-    this._subscribers.forEach((subscriber) => {
-      if (subscriber.fun) subscriber.fun(v);
-      else subscriber.ref[subscriber.name] = v;
-    });
-    this.proxy = v;
+    this._value = v;
+    this.notify();
   }
 
   get id(): string {
     return this._id;
   }
 
-  public subscriber(data: { ref: any; name: string; fun?: Function }) {
-    if (this._subscribers.indexOf(data) == -1) this._subscribers.push(data);
-    if (data?.fun == undefined) data.ref[data.name] = this.toString();
+  private notify() {
+    const remove: CustomEvent[] = [];
+    this._subscribers.forEach((subscriber) => {
+      if (!this.setValueToSubscriber(subscriber)) remove.push(subscriber);
+    });
+    this._subscribers = this._subscribers.filter(
+      (subscriber) => !remove.includes(subscriber),
+    );
+  }
+
+  private setValueToSubscriber(subscriber: CustomEvent): boolean {
+    try {
+      const target = subscriber.target?.deref();
+      if (target) {
+        if (subscriber.fun) {
+          const fun = subscriber.fun?.deref();
+          if (fun) fun(this._value);
+          else return false;
+        } else target[subscriber.propertyKey] = this._value;
+      } else return false;
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+
+  public subscriber(target: {}, propertyKey: string | symbol) {
+    const item = { target: new WeakRef(target), propertyKey };
+    if (this.setValueToSubscriber(item)) {
+      this._subscribers.push(item);
+      this.registry.register(item.target, item);
+    }
   }
 
   public unsubscribe(data: { ref: any; name: string; fun?: Function }) {
-    this._subscribers = this._subscribers.filter(
-      (subscriber) => subscriber != data,
-    );
+    // this._subscribers = this._subscribers.filter(
+    //   (subscriber) => subscriber != data,
+    // );
+  }
+
+  public onChange(fun: (value: T) => void) {
+    const data = new WeakRef(fun);
+    const item = { target: data, propertyKey: "fun", fun: data };
+    if (this.setValueToSubscriber(item)) {
+      this._subscribers.push(item);
+      this.registry.register(item.target, item);
+    }
+  }
+
+  toString(): string {
+    return this._value?.toString() || "";
   }
 }
-
-// declare global {
-
-//     interface Ref<T extends any> {
-//         subscriber(data: { ref: any, name: string }): void;
-//         unsubscribe(data: { ref: any, name: string }): void;
-//     }
-
-//     interface String {
-//         toRef(): Ref<string>;
-//     }
-//     interface Number {
-//         toRef(): Ref<number>;
-//     }
-
-//     interface Boolean {
-//         toRef(): Ref<boolean>;
-//     }
-
-//     interface Date {
-//         toRef(): Ref<Date>;
-//     }
-
-//     interface Array<T> {
-//         toRef(): Ref<T[]>;
-//         clear(): void;
-//     }
-
-//     interface Object {
-//         toRef(): Ref<Object>;
-//     }
-
-//     interface Function {
-//         toRef(): Ref<Function>;
-//     }
-
-//     interface Symbol {
-//         toRef(): Ref<Symbol>;
-//     }
-
-//     interface BigInt {
-//         toRef(): Ref<BigInt>;
-//     }
-
-//     interface RegExp {
-//         toRef(): Ref<RegExp>;
-//     }
-
-//     interface Error {
-//         toRef(): Ref<Error>;
-//     }
-
-//     interface EvalError {
-//         toRef(): Ref<EvalError>;
-//     }
-
-//     interface RangeError {
-//         toRef(): Ref<RangeError>;
-//     }
-
-//     interface ReferenceError {
-//         toRef(): Ref<ReferenceError>;
-//     }
-
-//     interface SyntaxError {
-//         toRef(): Ref<SyntaxError>;
-//     }
-
-//     interface TypeError {
-//         toRef(): Ref<TypeError>;
-//     }
-
-// }
-
-// Array.prototype.clear = function () {
-//     this.length = 0;
-// }
--- src/core/ref/index.ts
+++ src/core/ref/index.ts
@@ -1,3 +1,2 @@
-export { Ref2 } from "./Ref";
-export { Ref } from "./Ref2";
+export { Ref } from "./Ref";
 export { RefComponent } from "./RefComponent";
